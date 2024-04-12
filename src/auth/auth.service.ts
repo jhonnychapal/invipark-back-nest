47e3d29bd3879b44dc4ada78f714f1aff3889dee
Jhonny Chapal <jhonnychapal@gmail.com>
2024-04-11 20:06:51 -0500

    FEAT: create first record in DB

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+
+    try {
+      const newUser = new this.userModel(createUserDto);
+      
+      // 1. Encriptar la contraseña
+      // 2. Guardar el usuario
+      // 3. Generar el JWT
+
+      return await newUser.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} ya está registrado!`)
+      }
+      throw new InternalServerErrorException('Algo salió mal, contacta al admin!')
+
+    }
+
   }
 
   findAll() {
